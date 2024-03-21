30c5477d176d0531ddd68e0c33c3f8e0e1de52e6
Savitkumarchugh <savitkumarchugh@gmail.com>
2024-03-21 15:12:12 +0530

    initial

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Savit Kumar Chugh",
+    author_email="savitkumarchugh@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
